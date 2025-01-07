c8b8b1b5d1ccaf93cbb0753134e431e12e63e138
sebastian.duque <sebastianduque1323@hotmail.com>
2025-01-07 16:57:25 -0500

    bg update

--- sergio-benavides/components/Hero.tsx
+++ sergio-benavides/components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <section className="relative bg-green-900 text-white">
-      <div className="absolute inset-0 bg-[url('/background2.jpg')] bg-cover bg-center opacity-50 backdrop-blur-md"></div>
+      <div className="absolute inset-0 bg-[url('/background-2.jpg')] bg-cover bg-center opacity-50 backdrop-blur-md"></div>
       <div className="relative container mx-auto px-6 py-24 md:py-32">
       <Image src="/logo.jpg" alt="Benavides Escobar Law Logo" width={150} height={150} className="mb-8" />
       <h1 className="text-4xl md:text-6xl font-bold mb-4">Benavides Escobar Law</h1>
